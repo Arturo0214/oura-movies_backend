557140d62d8c22da56d234cea8e49a176d7b3227
Arturo0214 <osvaldosuarezcruz@gmail.com>
2023-05-11 13:33:20 -0600

    added a change on the setLikes route

--- backend/routes/movieRoutes.js
+++ backend/routes/movieRoutes.js
@@ -2,10 +2,9 @@ const express = require('express')
 const router = express.Router()
 const { getMovies, setMovie, updateMovie, deleteMovie, setLikes } = require('../controllers/movieController')
 const { protect } = require('../middleware/authMiddleware')
-const Movie = require('../models/movieModel')
 
 router.route('/').get(protect, getMovies).post(protect, setMovie)
 router.route('/:id').put(protect, updateMovie).delete(protect, deleteMovie)
-router.route('/:id/like').post(protect, setLikes)
+router.route('/:id/like').put(protect, setLikes)
 
 module.exports = router
